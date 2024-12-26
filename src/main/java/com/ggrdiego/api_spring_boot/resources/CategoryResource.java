b7c9ee7f74860adbf18d99f7392f0f05a6d9a118
dgeis <ggr.diego@gmail.com>
2024-12-26 13:47:11 -0300

    ServiceRepository

---
+++ src/main/java/com/ggrdiego/api_spring_boot/resources/CategoryResource.java
@@ -0,0 +1,36 @@
+package com.ggrdiego.api_spring_boot.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ggrdiego.api_spring_boot.entities.Category;
+import com.ggrdiego.api_spring_boot.services.CategoryService;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+	
+	@Autowired
+	private CategoryService service;
+	
+	@GetMapping(value="/tudo")
+	public ResponseEntity<List<Category>> findAll(){
+		List<Category> list = service.findAll();
+		
+		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value ="/{id}")
+	public ResponseEntity<Category> findById(@PathVariable Long id){
+		
+		Category obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+}
---
+++ src/main/java/com/ggrdiego/api_spring_boot/services/CategoryService.java
@@ -0,0 +1,31 @@
+package com.ggrdiego.api_spring_boot.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ggrdiego.api_spring_boot.entities.Category;
+import com.ggrdiego.api_spring_boot.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+
+	public List<Category> findAll() {
+
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+
+
+}
