ecca1682e24b44ca12f4a11a81ad3c234420eb5c
dgeis <ggr.diego@gmail.com>
2025-01-25 11:00:11 -0300

    userDelete DatabaseException

--- src/main/java/com/ggrdiego/api_spring_boot/services/UserService.java
+++ src/main/java/com/ggrdiego/api_spring_boot/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.ggrdiego.api_spring_boot.entities.User;
 import com.ggrdiego.api_spring_boot.repositories.UserRepository;
+import com.ggrdiego.api_spring_boot.services.exceptions.DatabaseException;
 import com.ggrdiego.api_spring_boot.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -33,7 +36,13 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 
 	public User update(long id, User obj) {
